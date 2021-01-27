e33994ad68fd7bcde50ba832f6e57c62d643ed82
khalidxx <fxtryio@gmail.com>
2021-01-27 23:56:41 +0800

    added image encoding endpoint

--- mainApp.py
+++ mainApp.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from tensorflow import keras
 import numpy as np
+import face_recognition
 
 app = Flask(__name__)
 model = keras.models.load_model("rating_model.h5")
@@ -9,12 +10,21 @@ model = keras.models.load_model("rating_model.h5")
 def running():
     return "Flask is running!"
 
+@app.route("/getEncoding", methods=["POST"])
+def Encode():
+    data = request.json
+    image = data["image"]
+
+    encoding = face_recognition.face_encodings(image)[0] # Rates only the first face in the image
+
+    return encoding
+
 @app.route("/rate", methods=["POST"])
 def rate():
     data = request.json
     data_sample = data["encoding"]
 
-    x = np.asarray(data_sample).astype('float32').reshape((1, 128))
+    x = np.asarray(data_sample).astype("float32").reshape((1, 128))
     x = x.tolist()
 
     prediction = model.predict(x)
@@ -22,5 +32,5 @@ def rate():
     return jsonify({"rating": str(prediction[0,0])})
     
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
