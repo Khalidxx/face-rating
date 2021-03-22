dfca4f8daeb56a8acb538158c16dc59998c05d73
khalidxx <fxtryio@gmail.com>
2021-03-23 00:21:54 +0800

    added rate button

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,33 +1,76 @@
 function readURL(input) {
-    if (input.files && input.files[0]) {
+  if (input.files && input.files[0]) {
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('.image-upload-wrap').hide();
+
+      $('.file-upload-image').attr('src', e.target.result);
+      $('.file-upload-content').show();
+
+      $('.image-title').html(input.files[0].name);
+
+      $(".rate-content").show();
+    };
+
+    reader.readAsDataURL(input.files[0]);
+
+  } else {
+    removeUpload();
+  }
+}
   
-      var reader = new FileReader();
+function removeUpload() {
+  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+  $('.file-upload-content').hide();
+  $(".rate-content").hide()
+  $('.image-upload-wrap').show();
+}
+
+$('.image-upload-wrap').bind('dragover', function () {
+  $('.image-upload-wrap').addClass('image-dropping');
+});
+
+$('.image-upload-wrap').bind('dragleave', function () {
+  $('.image-upload-wrap').removeClass('image-dropping');
+});
+
+function rateImage() {
+  var file = $(".file-upload-input")[0].files[0];
   
-      reader.onload = function(e) {
-        $('.image-upload-wrap').hide();
+  const fd = new FormData();
+  fd.append('image', file);
   
-        $('.file-upload-image').attr('src', e.target.result);
-        $('.file-upload-content').show();
-  
-        $('.image-title').html(input.files[0].name);
-      };
-  
-      reader.readAsDataURL(input.files[0]);
-  
-    } else {
-      removeUpload();
+
+  fetch(`${window.origin}/rate`, {
+    method: "POST",
+    body: fd,
+    cache: "no-cache",
+    // headers: new Headers({
+    //   "content-type": "application/json"
+    // })
+  })
+  .then(function(response) {
+    if (response.status !== 200) {
+      console.log(`Looks like there was a problem. Status code: ${response.status}`);
+      return;
     }
-  }
-  
-  function removeUpload() {
-    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-    $('.file-upload-content').hide();
-    $('.image-upload-wrap').show();
-  }
-  $('.image-upload-wrap').bind('dragover', function () {
-          $('.image-upload-wrap').addClass('image-dropping');
-      });
-      $('.image-upload-wrap').bind('dragleave', function () {
-          $('.image-upload-wrap').removeClass('image-dropping');
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  })
+  .catch(function(error) {
+    console.log("Fetch error: " + error);
   });
+
+
+
+  // if (file) {
+  //   reader.readAsDataURL(file);
+  //   console.log(file)
+  // } else {
+  //   preview.src = "";
+  // }
+}
   
